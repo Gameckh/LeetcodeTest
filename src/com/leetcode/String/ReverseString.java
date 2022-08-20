52e495ef54c8ff914986ce45c3be622f39e043c2
Gameckh <chenkaihua_kevin@foxmail.com>
2022-08-21 06:40:27 +0800

    string basic 1

---
+++ src/com/leetcode/String/ReverseString.java
@@ -0,0 +1,25 @@
+package com.leetcode.String;
+
+public class ReverseString {
+
+    /*
+    * 字符串类型题目的基础：反转字符串
+    * https://wizardforcel.gitbooks.io/the-art-of-programming-by-july/content/01.01.html
+    * 上文有两个重要知识点：
+    *   1. 如本题目的反转字符串算法
+    *   2. (X^TY^T)^T=YX
+    * */
+    public static void main(String[] args) {
+
+    }
+
+    public static void reverseString(char[] s) {
+        if(s == null || s.length <= 1) return;
+        int i = 0, j = s.length-1;
+        while(i < j) {
+            char c = s[i];
+            s[i++] = s[j];
+            s[j--] = c;
+        }
+    }
+}
