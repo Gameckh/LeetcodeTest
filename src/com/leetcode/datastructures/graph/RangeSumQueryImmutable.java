6b8ebe740f31379948291ebcadaf393ff11f3c01
Gameckh <Kevin.Chen@grapecity.com>
2022-09-21 06:58:22 +0800

    2022-9-21

---
+++ src/com/leetcode/datastructures/graph/RangeSumQueryImmutable.java
@@ -0,0 +1,30 @@
+package com.leetcode.datastructures.graph;
+
+public class RangeSumQueryImmutable {
+
+    /*
+    * https://leetcode.com/problems/range-sum-query-immutable/
+    *
+    * 思路：
+    * */
+    public static void main(String[] args) {
+
+    }
+
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+class NumArray {
+
+    public NumArray(int[] nums) {
+
+    }
+
+    public int sumRange(int left, int right) {
+        return 0;
+    }
+}
--- src/com/leetcode/datastructures/graph/ReconstructItinerary.java
+++ src/com/leetcode/datastructures/graph/ReconstructItinerary.java
@@ -1,5 +1,7 @@
 package com.leetcode.datastructures.graph;
 
+import java.util.*;
+
 public class ReconstructItinerary {
 
     /*
@@ -7,4 +9,42 @@ public class ReconstructItinerary {
     *
     * 思路：
     * */
+
+    public static void main(String[] args) {
+        String[][] tickets = {{"MUC","LHR"},{"JFK","MUC"},{"SFO","SJC"},{"LHR","SFO"}};
+        List<List<String>> listTickets = new ArrayList<>();
+        for (String[] strings : tickets) {
+            List<String> ticket = new ArrayList<>();
+            ticket.add(strings[0]);
+            ticket.add(strings[1]);
+            listTickets.add(ticket);
+        }
+        findItinerary(listTickets);
+        System.out.println(res);
+    }
+
+    static Map<String, PriorityQueue<String>> map = new HashMap<>();
+    static List<String> res = new ArrayList<>();
+
+    public static List<String> findItinerary(List<List<String>> tickets) {
+        buildMap(tickets);
+        dfs("JFK");
+        return res;
+    }
+
+    static void dfs(String node) {
+        PriorityQueue<String> clone = map.get(node);
+        while (clone != null && !clone.isEmpty()) {
+            dfs(clone.poll());
+        }
+        res.add(0, node);
+    }
+
+    static void buildMap(List<List<String>> tickets) {
+        for (List<String> list: tickets){
+            PriorityQueue<String> val = map.getOrDefault(list.get(0), new PriorityQueue<>());
+            val.add(list.get(1));
+            map.put(list.get(0), val);
+        }
+    }
 }
