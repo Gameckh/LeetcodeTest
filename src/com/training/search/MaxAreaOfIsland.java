a4c13dc5b8e4c4ee75a9080fa3729519522cfe25
Gameckh <chenkaihua_kevin@foxmail.com>
2022-10-31 07:52:50 +0800

    2022-10-31

---
+++ src/com/training/search/MaxAreaOfIsland.java
@@ -0,0 +1,35 @@
+package com.training.search;
+
+public class MaxAreaOfIsland {
+
+    public static void main(String[] args) {
+
+    }
+
+    private static int[] directions = {-1, 0, 1, 0, -1};
+    public static int maxAreaOfIsland(int[][] grid) {
+        int maxArea = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                maxArea = Math.max(maxArea, dfs(grid, i, j));
+            }
+        }
+        return maxArea;
+    }
+
+    private static int dfs(int[][] grid, int row, int col) {
+        if (grid[row][col] == 0) return 0;
+        grid[row][col] = 0;
+        int x, y, area = 1;
+
+        for (int i = 0; i < directions.length - 1; i++) {
+            x = row + directions[i];
+            y = col + directions[i + 1];
+            if (x >= 0 && x < grid.length && y >= 0 && y < grid[0].length) {
+                area += dfs(grid, x, y);
+            }
+        }
+
+        return area;
+    }
+}
--- src/com/training/sorts/leetcode/KthLargestElementInAnArray.java
+++ src/com/training/sorts/leetcode/KthLargestElementInAnArray.java
@@ -10,7 +10,7 @@ public class KthLargestElementInAnArray {
     * */
     public static void main(String[] args) {
         int[] nums = {3,2,1,5,6,4};
-        int k = 2;
+        int k = 3;
         System.out.println(findKthLargest(nums, k));
     }
 
@@ -19,19 +19,50 @@ public class KthLargestElementInAnArray {
     * https://leetcode.com/problems/kth-largest-element-in-an-array/solutions/2180600/a-guide-to-quick-select-java/
     *
     * In my own word, quick selection is:
-    *
+    * 1. Quick selection is another form of quick sort, they have almost same ideas.
+    * 2. Partition function:
+    *       <1> Each execution will put the given element(pIndex) in the correct position, and return its position.
+    *       <2> At the beginning, swap the pIndex and right first, then pIndex from left to right.
+    *       <3> The for loop start with left and end with right(closure).
+    *       <4> If num[i] less than or equals pivot value, then swap the pIndex and i, and pIndex increase one.
+    *       <5> Return pIndex-1, because the pIndex++ has redundant once.
+    * 3. Quick selection function:
+    *       <1> Select a random pIndex, then find its correct position, then compare with k.
+    *       <2> Check whether the range has only one element.
+    *       <3> Get a random index between the left and right.
+    *       <4> Call partition function and get the correct position of the element at pIndex.
+    *       <5> Check k and pIndex, then go to different ranges.
     * */
     public static int findKthLargest(int[] nums, int k) {
         return quickSelection(nums, 0, nums.length-1, nums.length-k);
     }
 
+    //
     private static int quickSelection(int[] nums, int left, int right, int k) {
-        int pivot = nums[left];
-        return 1;
+        if (left == right) return nums[left];
+        int pIndex = new Random().nextInt(right - left + 1) + left;
+
+        pIndex = partition(nums, left, right, pIndex);
+
+        if (pIndex == k) return nums[pIndex];
+        if (pIndex < k) return quickSelection(nums, pIndex+1, right, k);
+        return quickSelection(nums, left, pIndex-1, k);
     }
 
+    /*
+    * Swap a certain value(pIndex) to correct position, and return the position.
+    * */
     public static int partition(int[] nums, int left, int right, int pIndex) {
-        return 0;
+        int pivot = nums[pIndex];
+        swap(nums, pIndex, right);
+        pIndex = left;
+
+        for (int i = left; i <= right; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums, i, pIndex++);
+            }
+        }
+        return pIndex-1;
     }
 
     private static void swap(int[] nums, int i, int j) {
